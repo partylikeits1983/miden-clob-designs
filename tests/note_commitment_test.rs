f55f61474995bd3e7dda643250c3413a84886b43
riemann <aleqvids@gmail.com>
2025-03-20 12:29:39 -0400

    feat: input_ commitment PoC

---
+++ tests/note_commitment_test.rs
@@ -0,0 +1,107 @@
+use std::{fs, path::Path};
+
+use miden_client::{
+    note::{
+        Note, NoteAssets, NoteExecutionHint, NoteExecutionMode, NoteInputs, NoteMetadata,
+        NoteRecipient, NoteScript, NoteTag, NoteType,
+    },
+    transaction::{OutputNote, TransactionKernel, TransactionRequestBuilder},
+    ClientError, Felt, Word,
+};
+
+use miden_crypto::{hash::rpo::Rpo256 as Hasher, rand::FeltRng};
+
+use miden_clob_designs::common::{
+    create_basic_account, initialize_client, reset_store_sqlite, wait_for_notes,
+};
+
+#[tokio::test]
+async fn note_input_commitment_test() -> Result<(), ClientError> {
+    // reset store.sqlite file
+    reset_store_sqlite().await;
+
+    let mut client = initialize_client().await?;
+    println!(
+        "Client initialized successfully. Latest block: {}",
+        client.sync_state().await.unwrap().block_num
+    );
+
+    // -------------------------------------------------------------------------
+    // STEP 1: Create account
+    // -------------------------------------------------------------------------
+    println!("\n[STEP 1] Creating new accounts");
+    let alice_account = create_basic_account(&mut client).await?;
+    println!("Alice's account ID: {:?}", alice_account.id().to_hex());
+    let bob_account = create_basic_account(&mut client).await?;
+    println!("Bob's account ID: {:?}", bob_account.id().to_hex());
+
+    // -------------------------------------------------------------------------
+    // STEP 3: Create custom note
+    // -------------------------------------------------------------------------
+    println!("\n[STEP 3] Create custom note");
+    let mut secret_vals = vec![Felt::new(1), Felt::new(2), Felt::new(3), Felt::new(4)];
+    secret_vals.splice(0..0, Word::default().iter().cloned());
+    let digest = Hasher::hash_elements(&secret_vals);
+    println!("digest: {:?}", digest);
+
+    let assembler = TransactionKernel::assembler().with_debug_mode(true);
+    let code = fs::read_to_string(Path::new("./masm/notes/input_commitment_test.masm")).unwrap();
+    let rng = client.rng();
+    let serial_num = rng.draw_word();
+    let note_script = NoteScript::compile(code, assembler).unwrap();
+    let note_inputs = NoteInputs::new(digest.to_vec()).unwrap();
+    let recipient = NoteRecipient::new(serial_num, note_script, note_inputs);
+    let tag = NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
+    let metadata = NoteMetadata::new(
+        alice_account.id(),
+        NoteType::Public,
+        tag,
+        NoteExecutionHint::always(),
+        Felt::new(0),
+    )?;
+    let vault = NoteAssets::new(vec![])?;
+    let custom_note = Note::new(vault, metadata, recipient);
+    println!("note hash: {:?}", custom_note.hash());
+
+    let note_req = TransactionRequestBuilder::new()
+        .with_own_output_notes(vec![OutputNote::Full(custom_note.clone())])
+        .unwrap()
+        .build();
+    let tx_result = client
+        .new_transaction(alice_account.id(), note_req)
+        .await
+        .unwrap();
+    println!(
+        "View transaction on MidenScan: https://testnet.midenscan.com/tx/{:?}",
+        tx_result.executed_transaction().id()
+    );
+    let _ = client.submit_transaction(tx_result).await;
+    client.sync_state().await?;
+
+    // -------------------------------------------------------------------------
+    // STEP 4: Consume the Custom Note
+    // -------------------------------------------------------------------------
+    wait_for_notes(&mut client, &bob_account, 1).await?;
+    println!("\n[STEP 4] Bob consumes the Note");
+    // let secret = [Felt::new(1), Felt::new(2), Felt::new(3), Felt::new(4)];
+
+    let inputs: NoteInputs = NoteInputs::new(vec![Felt::new(2), Felt::new(1)]).unwrap();
+
+    println!("input commitment: {:?}", inputs.commitment());
+
+    let consume_custom_req = TransactionRequestBuilder::new()
+        .with_authenticated_input_notes([(custom_note.id(), None)])
+        .build();
+    let tx_result = client
+        .new_transaction(bob_account.id(), consume_custom_req)
+        .await
+        .unwrap();
+    println!(
+        "Consumed Note Tx on MidenScan: https://testnet.midenscan.com/tx/{:?}",
+        tx_result.executed_transaction().id()
+    );
+    println!("account delta: {:?}", tx_result.account_delta().vault());
+    let _ = client.submit_transaction(tx_result).await;
+
+    Ok(())
+}
